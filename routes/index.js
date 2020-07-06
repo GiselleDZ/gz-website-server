949680f0b1fddb581ed48fe3c25488f538e4cb40
GiselleDZ <giselle@gisellezatonyl.com>
2020-07-05 20:09:22 -0400

    reroute home

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.send('unauthorized: please visit gisellezatonyl.com');
 });
 
 module.exports = router
